fde0a15d3d06754981cbf2206bd887686f0d7c3a
Tom Tonic <tom@tomtonic.de>
2025-10-11 22:28:56 +0200

    Refactor time measurement functions for improved precision and clarity

--- sampletime_other.go
+++ sampletime_other.go
@@ -6,12 +6,12 @@ import "time"
 
 //import "golang.org/x/sys/unix"
 
+// A relative TimeStamp with the highest possible precision on the current runtime system.
+// The values aren't comparable between computer restarts or between computers.
+// They are only comparable on the same computer between two calls to SampleTime() within the same runtime of a program.
 type TimeStamp = time.Time
 
-// Now returns current TimeStamp with best possible precision.
-//
-// Now returns time offset from a specific time.
-// The values aren't comparable between computer restarts or between computers.
+// SampleTime returns a timestamp with the highest possible precision on the current runtime system.
 func SampleTime() TimeStamp {
 	//return time.Now().UnixNano()
 	return time.Now()
@@ -23,7 +23,9 @@ func SampleTime() TimeStamp {
 	*/
 }
 
-// Retruns the difference between two timestams in nanoseconds with the highest possible precision (which might be more than just a nanosecond).
+// Retruns the difference between two timestams in nanoseconds with the highest possible precision (which might be more than just one nanosecond).
+// The function assumes that t_later is later than t_earlier and will return a negative value if this is not the case.
+// Please note that the call to this function does NOT have constant runtime on other systems but Windows.
 func DiffTimeStamps(t_earlier, t_later TimeStamp) int64 {
 	result := t_later.Sub(t_earlier)
 	return result.Nanoseconds()
