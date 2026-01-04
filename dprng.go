16c97e6996378613d42b2a071df3081d46aef620
Tom Tonic <tom@tomtonic.de>
2026-01-04 20:17:17 +0100

    Implement scrambler support and add related tests. Improve Uint32N-performance

--- dprng.go
+++ dprng.go
@@ -1,6 +1,9 @@
 package rtcompare
 
-import "math/rand"
+import (
+	"math/bits"
+	"math/rand"
+)
 
 // DPRNG is a Deterministic Pseudo-Random Number Generator based on the xorshift* algorithm
 // (see https://en.wikipedia.org/wiki/Xorshift#xorshift*).
@@ -12,25 +15,53 @@ import "math/rand"
 // This random number generator has a very small memory footprint (16 bytes).
 // The initial state must not be zero.
 type DPRNG struct {
-	State uint64
-	Round uint64 // for debugging purposes
+	State     uint64
+	Scrambler uint64
+	Round     uint64 // for debugging purposes
 }
+
+const goldenRatio64 = uint64(0x9E3779B97F4A7C15) // (sqrt(5)-1)/2 * 2^64
+const vigna = uint64(0x2545F4914F6CDD1D)         // Vigna's default scrambler constant optimized for our 12/25/27 xorshift
 
 // NewDPRNG creates a new Deterministic Pseudo-Random Number Generator instance.
 // If no seed is provided, it initializes the state with a random non-zero value.
 // If the provided seed is zero, it initializes the state with a random non-zero value.
 // Otherwise, it uses the provided seed value.
+// If a second uint64 value is provided, it is used as the scrambler constant. The scrambler
+// constant creates a permutation (different sequence) of the generated numbers. If no scrambler
+// constant is provided, Vigna's default scrambler constant is used.
+// The only requirement for the scrambler constant is that it must be an odd number to ensure
+// maximal period, but this is automatically enforced by the code. You can use [GenerateScrambler]
+// to generate good quality scrambler constants.
 func NewDPRNG(seed ...uint64) DPRNG {
 	result := DPRNG{}
 	if len(seed) == 0 {
 		result.State = uint64(rand.Uint64()&0xFFFFFFFFFFFFFFE + 1) // initialize with a random number != 0
+		result.Scrambler = vigna
 	} else {
 		result.State = seed[0]
 		if result.State == 0 {
 			result.State = uint64(rand.Uint64()&0xFFFFFFFFFFFFFFE + 1) // initialize with a random number != 0
 		}
+		if len(seed) > 1 {
+			result.Scrambler = seed[1] | 1 // ensure scrambler is odd
+		} else {
+			result.Scrambler = vigna
+		}
 	}
 	return result
+}
+
+// GenerateScrambler generates good quality scrambler constants for the DPRNG. The number of iterations
+// does not influence the quality of the scrambler constant, but each iteration generates a different
+// scrambler constants in a deterministic way. You can use the returned scrambler constant when creating
+// a new DPRNG instance to get a different permutation (i.e., sequence) of generated numbers.
+func GenerateScrambler(iterations uint64) uint64 {
+	s := goldenRatio64
+	for range iterations {
+		s += goldenRatio64
+	}
+	return s
 }
 
 // This function returns the next pseudo-random number in the sequence.
@@ -42,7 +73,7 @@ func (thisState *DPRNG) Uint64() uint64 {
 	x ^= x >> 27
 	thisState.State = x
 	thisState.Round++
-	return x * 0x2545F4914F6CDD1D
+	return x * thisState.Scrambler
 }
 
 // Float64 returns a pseudo-random float64 in the range [0.0, 1.0) like Go’s math/rand.Float64().
@@ -61,8 +92,8 @@ func (thisState *DPRNG) Float64() float64 {
 // Note: This implementation may introduce a slight bias if n is not a power of two.
 func (thisState *DPRNG) UInt32N(n uint32) uint32 {
 	u64 := thisState.Uint64()
-	l32 := u64 & 0xFFFFFFFF
-	prod := l32 * uint64(n)
-	result := uint32(prod >> 32)
-	return result
+	hi, _ := bits.Mul64(u64, uint64(n))
+	// we only need the high 64 bits, which is equivalent to (u64 * n) >> 64
+	// as n <= 2^32, hi is guaranteed to fit into 32 bits
+	return uint32(hi)
 }
